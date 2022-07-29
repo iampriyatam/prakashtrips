f2e94bce6b2138a8d74c5786e52ddf37db3a98a7
Priyatam <priyatam31260@gmail.com>
2022-07-29 11:04:54 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+//Get the button
+const mybutton = document.getElementById("myBtn");
+const nav = document.getElementById("header");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > nav.offsetHeight || document.documentElement.scrollTop > nav.offsetHeight) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
